68443f816cad69765d252cfa05b42f1fa588d49c
Studenckie Koło Naukowe Gamedev PBŚ <150017430+KNTGpbs@users.noreply.github.com>
2024-10-27 11:02:12 +0100

    DywanCommit

---
+++ Assets/Scripts/CarpetTrigger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CarpetTrigger : MonoBehaviour
+{
+    private ItemContainer itemContainer;
+
+    private PlayerMovement player;
+    private SpriteRenderer sr;
+
+    [SerializeField] private Sprite usedCarpet;
+
+    [SerializeField] private GameObject plank;
+    private bool playerEntered = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        itemContainer = gameObject.GetComponent<ItemContainer>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        plank = GameObject.Find("deska");
+        plank.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !itemContainer.IsLocked && player.GetTurnedToBG() && !itemContainer.GetUsed() && playerEntered)
+        {
+            sr.sprite = usedCarpet;
+            plank.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerEntered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerEntered = false;
+    }
+}
